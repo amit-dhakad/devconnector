7cb929fbc38d00b731e83482a2a88a841ef11a1f
amit-dhakad <amit0502dhakad@gmail.com>
2020-09-18 17:23:12 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+const app = express();
+// DB Config
+
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDb Connected');
+  })
+  .catch((err) => console.error(err));
+
+app.get('/', (req, res) => res.send('hello world'));
+
+// Use Routes
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on ${port}`));
