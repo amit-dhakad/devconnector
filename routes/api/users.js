d6cf335611efced029552e0363d2312f86ec5948
amit-dhakad <amit0502dhakad@gmail.com>
2020-09-19 15:06:33 +0530

    added validation for login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,11 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //@route GET api/users/test
 //@desc Tests users route
@@ -17,10 +22,16 @@ router.get('/test', (req, res) => res.json({ msg: 'User works' }));
 //@access Public
 
 router.post('/register', (req, res) => {
-  console.log('req: body ', req.body.email);
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      return res.status(400).json({ email: 'Email alrady exits' });
+      errors.email = 'Email already exits';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //Size
@@ -53,12 +64,20 @@ router.post('/register', (req, res) => {
 // @access Public
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   User.findOne({ email }).then((user) => {
     if (!user) {
-      return res.status(404).json({ email: 'User not found' });
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -74,9 +93,26 @@ router.post('/login', (req, res) => {
           });
         });
       } else {
-        return res.status(400).json({ password: 'Password incorrect' });
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
 });
+
+// @route GET api/users/current
+// @desc Return current user
+// @access Private
+
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  }
+);
 module.exports = router;
