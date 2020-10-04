30f1965335a74b76c168d352f93b3399b3cdd45d
amit-dhakad <amit0502dhakad@gmail.com>
2020-10-04 11:11:41 +0530

    added comment form

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { getPost } from '../../actions/postActions';
 import Spinner from '../common/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 class Post extends Component {
   componentDidMount() {
     this.props.getPost(this.props.match.params.id);
@@ -19,6 +20,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
