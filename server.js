7ee88d4b6c3b0f658f43d811f55efcc0200cc065
amit-dhakad <amit0502dhakad@gmail.com>
2020-10-05 15:19:25 +0530

    removed console

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const db = require('./config/keys').mongoURI;
-console.log('tteest');
 // Connect to MongoDB
 
 mongoose
@@ -44,11 +43,9 @@ app.use('/api/posts', posts);
 //  Server static assets if in production
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('==============1111');
   // Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    console.log('22222222222222222');
     res.sendFile(path.resilve(__dirname, 'client', 'build', 'index.html'));
   });
 }
